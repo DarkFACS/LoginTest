e5401b78908c485b79a47210aaf1a735fe871bcd
DarkFACS <facs8813@gmail.com>
2019-09-08 00:44:07 -0500

    My first commit.

---
+++ src/app/layout/bs-element/bs-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-element',
+  templateUrl: './bs-element.component.html',
+  styleUrls: ['./bs-element.component.css']
+})
+export class BsElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
